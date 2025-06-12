c029e29057f71b0a93a7f173241f8a1c5816aba4
smypmsa <antonsauchyk@gmail.com>
2025-06-12 21:32:08 +0000

    feat: add default rpc urls

--- src/common/config.py
+++ src/common/config.py
@@ -7,8 +7,19 @@ load_dotenv(override=True)
 
 class Settings:
     def __init__(self):
+        # Set default RPC URLs
+        self.ARBITRUM_RPC_URL = os.getenv("ARBITRUM_RPC_URL", "https://arb1.arbitrum.io/rpc")
+        self.BASE_RPC_URL = os.getenv("BASE_RPC_URL", "https://mainnet.base.org")
+        self.BINANCE_SMART_CHAIN_RPC_URL = os.getenv(
+            "BINANCE_SMART_CHAIN_RPC_URL", "https://bsc-dataseed.binance.org"
+        )
+        self.ETHEREUM_RPC_URL = os.getenv("ETHEREUM_RPC_URL", "https://eth.llamarpc.com")
+        self.SONIC_RPC_URL = os.getenv("SONIC_RPC_URL", "https://rpc.soniclabs.com")
+        self.SOLANA_RPC_URL = os.getenv("SOLANA_RPC_URL", "https://api.mainnet-beta.solana.com")
+
         for key, value in os.environ.items():
-            setattr(self, key, value)
+            if not hasattr(self, key):
+                setattr(self, key, value)
 
 
 settings = Settings()
